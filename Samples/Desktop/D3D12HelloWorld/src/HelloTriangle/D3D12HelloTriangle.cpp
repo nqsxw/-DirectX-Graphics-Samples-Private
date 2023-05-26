a2c5b91a2bb411bf61b3ae135e8559b3f47f5d1f
Jianhua Wu <jianhuaw@nvidia.com>
2023-05-26 11:37:16 +0800

    Update v142 to v143
    Signed-off-by: Jianhua Wu <jianhuaw@nvidia.com>

--- Samples/Desktop/D3D12HelloWorld/src/HelloTriangle/D3D12HelloTriangle.cpp
+++ Samples/Desktop/D3D12HelloWorld/src/HelloTriangle/D3D12HelloTriangle.cpp
@@ -35,16 +35,16 @@ void D3D12HelloTriangle::LoadPipeline()
 #if defined(_DEBUG)
     // Enable the debug layer (requires the Graphics Tools "optional feature").
     // NOTE: Enabling the debug layer after device creation will invalidate the active device.
-    {
-        ComPtr<ID3D12Debug> debugController;
-        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-        {
-            debugController->EnableDebugLayer();
+    //{
+    //    ComPtr<ID3D12Debug> debugController;
+    //    if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+    //    {
+    //        debugController->EnableDebugLayer();
 
-            // Enable additional debug layers.
-            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-        }
-    }
+    //        // Enable additional debug layers.
+    //        dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+    //    }
+    //}
 #endif
 
     ComPtr<IDXGIFactory4> factory;
